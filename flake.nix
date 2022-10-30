aa6beeb9ac7ea67161ac6f67fbebe5c8b9fb5118
Berry Phillips <berry@berryp.xyz>
2022-10-30 22:01:06 +0900

    Add Python

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.devshell.url = "github:numtide/devshell";
+
+  outputs = { self, nixpkgs, devshell }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+      overlays = [ devshell.overlay ];
+    in
+    {
+      packages = forAllSystems (system: {
+        default = pkgs.${system}.poetry2nix.mkPoetryApplication { projectDir = self; };
+      });
+
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.devshell.mkShell {
+          packages = with pkgs.${system}; [
+            (poetry2nix.mkPoetryEnv { projectDir = self; })
+            poetry
+          ];
+        };
+      });
+    };
+}
